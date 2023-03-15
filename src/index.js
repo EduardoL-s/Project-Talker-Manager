dc12c5c7ef4a529d71d2330712e125ff43dce368
EduardoL-s <eduardoluizmm@hotmail.com>
2023-03-15 15:45:19 -0300

    [Requisito 2] Criação de um endpoint /GET para a rota talker/:id que retorna os dados do palestrante caso o id seja válido, e uma mensagem de aviso caso não seja

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 
+const talker = require('./talker.json');
+
 const app = express();
 app.use(express.json());
 
@@ -9,6 +11,20 @@ const PORT = process.env.PORT || '3001';
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
+});
+
+app.get('/talker', (req, res) => {
+  res.status(200).json(talker);
+});
+
+app.get('/talker/:id', (req, res) => {
+  const { params: { id: urlId } } = req;
+  const palestrante = talker.find(({ id }) => id === Number(urlId));
+  if (palestrante) {
+    res.status(200).json(palestrante);
+  } else {
+    res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+  }
 });
 
 app.listen(PORT, () => {
