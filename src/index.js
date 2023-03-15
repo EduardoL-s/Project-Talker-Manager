a092cfc3b056d135e3c816284d3b75ce01e5d9c5
EduardoL-s <eduardoluizmm@hotmail.com>
2023-03-15 16:38:03 -0300

    [Requisito 3] Criação do endpoint /POST que gera um token aleatório de 16 caracteres ao inserir as informações de login

--- src/index.js
+++ src/index.js
@@ -27,6 +27,13 @@ app.get('/talker/:id', (req, res) => {
   }
 });
 
+app.post('/login', (req, res) => {
+  // Método de geração aleatória de token encontrado no site: https://medium.com/@norbertofariasmedeiros/five-steps-como-gerar-um-random-token-em-javascript-1e1488a15d28
+  const randomToken = Math.random().toString(16).substr(2) + Math.random().toString(16).substr(2);
+  const tokenG = randomToken.slice(0, 16);
+  res.status(200).json({ token: tokenG });
+});
+
 app.listen(PORT, () => {
   console.log('Online');
 });
